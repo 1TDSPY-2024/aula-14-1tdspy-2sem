2a980ebcf9a7c59ff72ffbae0f10027d43746adc
matheusGmota <livematgreg@gmail.com>
2024-10-04 19:56:41 -0300

    Criando o loading e template com framer-motion, integrando com layout template

--- aula14/src/components/Cabecalho/Cabecalho.tsx
+++ aula14/src/components/Cabecalho/Cabecalho.tsx
@@ -2,7 +2,7 @@ import Menu from "../Menu/Menu";
 
 export default function Cabecalho() {
     return (
-        <header className="prose lg:prose-xl">
+        <header className="prose lg:prose-xl max-w-screen flex">
             <h1>Cabecalho</h1>
             <Menu />
         </header>
--- aula14/src/components/Menu/Menu.tsx
+++ aula14/src/components/Menu/Menu.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Menu() {
     return (
         <nav className='menu'>
-            <ul>
+            <ul className='w-full flex'>
                 <li> <Link href="/">Home</Link> </li>
                 <li> <Link href="/produtos">Produtos</Link> </li>
             </ul>
