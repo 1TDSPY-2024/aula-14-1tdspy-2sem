ad4c76c85c976afac1eb0cfbc58114d8edab60c1
viniciuslabernardes <vinicius.l.a.bernardes@outlook.com>
2024-10-04 20:54:41 -0300

    aula14

--- aula14/src/app/api/base-produtos/route.tsx
+++ aula14/src/app/api/base-produtos/route.tsx
@@ -6,3 +6,14 @@ export async function GET() {
     const dados = JSON.parse(file);
     return NextResponse.json(dados);
 }
+export async function POST(request:Request){
+
+    //Recebendo o request do cliente:
+    const{nome,idade} = await request.json();
+
+    console.log("Nome: ",nome)
+    console.log("Idade: ",idade)
+
+    return NextResponse.json({msg:"Sucesso"},{status:201});
+
+}
